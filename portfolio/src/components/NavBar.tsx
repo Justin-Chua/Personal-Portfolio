3df5f2fad2d8e35be26d657d646abf44b72f08b1
Justin-Chua <justin.chua@ucalgary.ca>
2024-07-30 17:21:49 -0600

    added hover animation for nav

--- portfolio/src/components/NavBar.tsx
+++ portfolio/src/components/NavBar.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 import signature from "../resources/signature.png";
 
 const NavBar = () => {
     return (
-        <nav className="flex bg-gray-700 p-6 w-full h-16 items-center justify-between">
+        <nav className="flex bg-green-900 p-8 w-full h-16 items-center justify-between">
             <div className="flex-none">
                 { /** Clicking on the image should bring you to the top of the page */}
-                <Image src={signature} alt="signature" className = "max-w-48"/>
+                <Image src={signature} alt="signature" className="max-w-48"/>
             </div>
-            <div className="flex gap-10 font-koulen">
-                <div><a>Home</a></div>
-                <div><a>About</a></div>
-                <div><a>Skills</a></div>
-                <div><a>Projects</a></div>
-                <div><a>Resume</a></div>
-                <div><a>Contact</a></div>
+            <div className="flex gap-10 font-koulen text-4xl text-white">
+                { /** Each anchor should bring you to a certain position on the page */}
+                <a id="nav-home" className="nav-hover">Home</a>
+                <a id="nav-about" className="nav-hover">About</a>
+                <a id="nav-skills" className="nav-hover">Skills</a>
+                <a id="nav-projects" className="nav-hover">Projects</a>
+                <a id="nav-resume" className="nav-hover">Resume</a>
+                <a id="nav-contact" className="nav-hover">Contact</a>
             </div>
         </nav>
     );
