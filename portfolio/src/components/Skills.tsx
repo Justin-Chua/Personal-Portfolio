15af06a096190d6c8c8af5a506026b6ece63f64b
Justin-Chua <justin.chua@ucalgary.ca>
2024-10-01 17:17:37 -0600

    added custom icons for skills section

--- portfolio/src/components/Skills.tsx
+++ portfolio/src/components/Skills.tsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const Skills = () => {
     return (
-        <div className="wrapper">
-            <p>whats up yall</p>
+        <div className="wrapper pt-16">
+            <div className="flex flex-col gap-y-16 font-koulen text-6xl text-amber-950 items-center justify-center">
+                <div className="flex gap-x-8">
+                    <h1>Languages</h1>
+                </div>
+                <div className="flex">
+                    <p>Frameworks</p>
+                </div>
+                <div className="flex">
+                    <p>Development Tools</p>
+                </div>
+            </div>
         </div>
     );
 };
