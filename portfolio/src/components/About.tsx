52d58f62fa225d790d19ee210067fa47f3f0087e
Justin-Chua <justin.chua@ucalgary.ca>
2025-01-29 19:27:46 -0700

    applied block to style directly rather than className

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -64,7 +64,7 @@ const About: React.FC = () => {
                 onClick={() => changeSlide('decrement')}
                 className='hover:brightness-50'
               >
-                <KeyboardArrowLeft color='inherit' fontSize='inherit' className='block' />
+                <KeyboardArrowLeft color='inherit' fontSize='inherit' style={{display: 'block'}} />
               </button>
               {aboutItems.map((_, index) => (
                 <button 
@@ -80,7 +80,7 @@ const About: React.FC = () => {
                 onClick={() => changeSlide('increment')}
                 className='hover:brightness-50'
               >
-                <KeyboardArrowRight color='inherit' fontSize='inherit' className='block' />
+                <KeyboardArrowRight color='inherit' fontSize='inherit' style={{display: 'block'}} />
               </button>
             </div>
           </div>
